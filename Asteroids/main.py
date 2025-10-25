87f125ba252dfd2ae780fb232831cdaba0109533
Jacob Snyder <95860834+jsGH314@users.noreply.github.com>
2025-10-24 22:57:32 -0500

    Created player class, CircleShape class, and an override of CircleShape's draw method in the player class.

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids Game...")
@@ -11,6 +12,11 @@ def main():
     #Create the game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     #Game loop
     while True:
         #Handle events, like quitting the game
@@ -19,8 +25,17 @@ def main():
                 return
         #Start with a black screen
         screen.fill((0, 0, 0))
+
+        #Draw the player
+        player.draw(screen)
+
         #Update the display
         pygame.display.flip()
+
+        #It will pause the game loop until 1/60th of a second has passed.
+        #Caps the frame rate at 60 FPS and calculates the time delta
+        #Divided by 1000 to convert milliseconds to seconds
+        dt = clock.tick(60)/1000
 
 
 
---
+++ Asteroids/player.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+from CircleShape import CircleShape
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        # Add a rotation field
+        self.rotation = 0
+
+    # Draw the player as a triangle
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    # A player will look like a triangle, 
+    # even though we'll use a circle to represent its hitbox.
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
